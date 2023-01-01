0f6f62ad794cca749991a47d28a937c039ce1e98
Denis Tavabilov <dagger1233@gmail.com>
2023-01-02 01:50:17 +0500

    govnocod form

--- src/App.vue
+++ src/App.vue
@@ -1,43 +1,72 @@
 <script setup>
-import { ref, reactive, computed } from 'vue'
-
-const state = reactive({
-  count: 0,
-  text: '',
-  data: [
-    { id: 1, name: 'Denis', age: 32 },
-    { id: 2, name: 'Anya', age: 22 },
-    { id: 3, name: 'Kostya', age: 1 },
-    { id: 4, name: 'Rinat', age: 50 },
-    { id: 5, name: 'Roza', age: 70 },
-  ],
+import { reactive, computed, ref } from 'vue'
+import { EMPTY_STRING } from './common/constants'
+import axios from 'axios'
+const review = reactive({
+  author: EMPTY_STRING,
+  stars: null,
+  text: EMPTY_STRING,
+  photo: null,
+  isRecommended: false,
 })
-const onButtonIncreaseCountClick = () => (state.count += 1)
-const onButtonDicreaseCountClick = () => (state.count -= 1)
-const queryData = computed(() => {
-  return state.data.filter(
-    (item) =>
-      item.name.toLowerCase().indexOf(state.text.toLowerCase()) >= 0 ||
-      item.age >= state.text
-  )
+const stars = [1, 2, 3, 4, 5]
+const previewFilePath = computed(() => {
+  if (review.photo) return URL.createObjectURL(review.photo)
 })
-console.log(queryData)
+const submit = () => {
+  console.log('Submit!')
+  axios
+    .post('api', review, {
+      headers: {
+        'Content-Type': 'multipart/form-data',
+      },
+    })
+    .then((res) => {
+      console.log(res)
+    })
+    .catch((err) => {
+      console.log('Error!!!!!', err)
+    })
+}
+const uploadFile = (e) => {
+  const file = e.target.files[0]
+  review.photo = file
+}
 </script>
 
 <template>
-  <div>
-    {{ state.count }}
+  <form action="#">
+    <el-input v-model="review.author" placeholder="Please input" clearable />
+
+    <el-input
+      v-model="review.text"
+      :rows="2"
+      type="textarea"
+      placeholder="Please input"
+    />
+
+    <h4>Оценка</h4>
+    <el-radio-group v-model="review.stars">
+      <el-radio v-for="(star, index) in stars" :key="star" :label="index + 1">{{
+        star
+      }}</el-radio>
+    </el-radio-group>
+
     <div>
-      <button v-on:click="onButtonIncreaseCountClick">increase</button>
-      <button v-on:click="onButtonDicreaseCountClick">dicrease</button>
+      <input type="file" @change="uploadFile" />
+      <img
+        v-if="review.photo"
+        :src="previewFilePath"
+        alt="avatar"
+        style="width: 100px"
+      />
     </div>
-    <div>
-      <ul>
-        <li v-for="items in queryData" v-bind:key="items.id">
-          {{ items.name }} {{ items.age }}
-        </li>
-        <input type="text" v-model="state.text" />
-      </ul>
-    </div>
-  </div>
+
+    <el-radio-group v-model="review.isRecommended">
+      <el-radio :label="false">Не советую</el-radio>
+      <el-radio :label="true">Советую</el-radio>
+    </el-radio-group>
+
+    <el-button type="primary" @click="submit">Отправить</el-button>
+  </form>
 </template>
